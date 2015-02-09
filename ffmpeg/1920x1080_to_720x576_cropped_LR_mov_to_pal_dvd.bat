0a5956a1aafb319eba3ecfd4a494a0e13cbb9990
Ananda Mahto <mrdwab@gmail.com>
2015-02-10 00:39:25 +0530

    tidying to multi-line

--- ffmpeg/1920x1080_to_720x576_cropped_LR_mov_to_pal_dvd.bat
+++ ffmpeg/1920x1080_to_720x576_cropped_LR_mov_to_pal_dvd.bat
@@ -1,2 +1,5 @@
-for %%a in ("*.mov") do ffmpeg -i "%%a" -aspect 4:3 -vf scale=1024x576,crop=iw-304:ih:152:0 -target pal-dvd "PAL_DVD_Format\%%~na.mpg"
+for %%a in ("*.mov") do ffmpeg -i "%%a"^
+ -aspect 4:3^
+ -vf scale=1024x576,crop=iw-304:ih:152:0^
+ -target pal-dvd "PAL_DVD_Format\%%~na.mpg"
 pause
