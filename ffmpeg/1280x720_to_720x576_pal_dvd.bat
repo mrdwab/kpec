f507118d21a0a0d29e71742e711a993843e0fe68
Ananda Mahto <mrdwab@gmail.com>
2015-02-10 10:38:31 +0530

    improved legibility

--- ffmpeg/1280x720_to_720x576_pal_dvd.bat
+++ ffmpeg/1280x720_to_720x576_pal_dvd.bat
@@ -1,2 +1,4 @@
-for %%a in ("*.avi") do ffmpeg -i "%%a" -aspect 4:3 -vf scale=720x404,pad=720:576:0:86:black -target pal-dvd "newfiles\%%~na.mpg"
+for %%a in ("*.avi") do ffmpeg -i "%%a"^
+ -aspect 4:3 -vf scale=720x404,pad=720:576:0:86:black^
+ -target pal-dvd "newfiles\%%~na.mpg"
 pause
